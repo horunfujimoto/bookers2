7ed60ad3b41b56cfa6ea6dcf6283443c8ca47c5c
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-08-13 15:21:37 +0000

    [Add]課題3:books・usersコントローラー作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  private
+  def book_params
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to:'homes#top'
   devise_for :users
+  #get homes/about => 'homes#about'
+  resources :books, only: [:index, :show] #ルーティングを一括して自動生成してくれる機能
+  resources :users, only: [:index, :show, :edit]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
