3b8cccbd16918630c836bf1730a919c833d5eb81
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-08-14 03:35:50 +0000

    [Add]課題3:ヘッダー編集、ログイン設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to:'homes#top'
   devise_for :users
-  #get homes/about => 'homes#about'
+  get "home/about" => "homes#about", as: "about"
   resources :books, only: [:index, :show] #ルーティングを一括して自動生成してくれる機能
   resources :users, only: [:index, :show, :edit]
 
