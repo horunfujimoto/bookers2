c9c99760a0c768186f646b537884c2e9e1235411
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-08-16 09:05:30 +0000

    [Add]課題3:usersのindex,showページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
 
   def index
+    @new_book = Book.new
+    @users = User.all
+    @user = current_user
+  end
 
+  def create
+    @book = Book.new(book_params) #これから作成するbook
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def show
-
+    @user = User.find(params[:id]) #params特定のレコードをとってくる！
+    @books = @user.books #表示したいbookは複数のためbooks
   end
 
   def edit
