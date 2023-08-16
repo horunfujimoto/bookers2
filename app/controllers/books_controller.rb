39ba5a984f660e3d959047dadb7436c4c36d3204
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-08-16 06:24:20 +0000

    [Add]課題3:booksのindex,showページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,40 @@
 class BooksController < ApplicationController
   def index
-
+    @new_book = Book.new
+    @books = Book.all
+    @user = current_user
   end
 
   def create
-
+    @book = Book.new(book_params) #これから作成するbook
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def show
+    @book = Book.find(params[:id]) #params特定のレコードをとってくる！
+    @user = @book.user
+    @new_book = Book.new #新規作成したbook
+  end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
   def book_params
+    params.require(:book).permit(:title, :body)
+  end
 
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to:'homes#top'
   devise_for :users
   get "home/about" => "homes#about", as: "about"
-  resources :books, only: [:index, :show] #ルーティングを一括して自動生成してくれる機能
+  resources :books, only: [:index, :new, :create, :show, :edit, :destroy] #ルーティングを一括して自動生成してくれる機能
   resources :users, only: [:index, :show, :edit]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
