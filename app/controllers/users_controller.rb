d79fbd29f1af94c1b61008070c649cbdfdc0352b
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-08-16 09:35:17 +0000

    [Add]課題3:usersのeditページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,17 +19,22 @@ class UsersController < ApplicationController
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def update
-
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id) #インスタンス変数(@user)ではない,(user.id)がなくでも動作にエラーでなかった
   end
 
   private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 
   def user_params
-
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get "home/about" => "homes#about", as: "about"
   resources :books, only: [:index, :new, :create, :show, :edit, :update, :destroy] #ルーティングを一括して自動生成してくれる機能
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
